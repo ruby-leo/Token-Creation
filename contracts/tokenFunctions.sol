68fd150e81c00fccbd3e228a912804f439ad9ba9
ruby-leo <nithyasen39@gmail.com>
2023-04-26 19:02:27 +0530

    Changed the Token Abbreviation to MTA from MTC

--- contracts/tokenFunctions.sol
+++ contracts/tokenFunctions.sol
@@ -19,7 +19,7 @@ contract MyToken {
 
     // public variables here
     string public tokenName = 'META';
-    string public tokenAbbrv = 'MTC';
+    string public tokenAbbrv = 'MTA';
     uint public totalSupply = 0;
 
     // mapping variable here
